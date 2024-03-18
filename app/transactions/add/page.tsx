578f3f948744ed123a131fb599d8392ba49fe505
João Victor <joaovi.15.rodrigues@gmail.com>
2024-03-17 22:17:49 -0300

    WIP

--- app/transactions/add/page.tsx
+++ app/transactions/add/page.tsx
@@ -1,28 +1,47 @@
+
+'use client'
 import Header from "@/app/components/Header";
+import React, { useState, useRef } from 'react';
+
 
 export default function Add()
 {
+    const [value, setValue] = useState( '0' );
+    const inputRef = useRef<HTMLInputElement>( null );
+
+    const handleChange = ( e: React.ChangeEvent<HTMLInputElement> ) =>
+    {
+        setValue( e.target.value );
+    };
     return (
         <div className="flex flex-col min-h-scre  ">
             <Header title="Registrar Gasto"></Header>
-            <div className="flex gap-2">
+            <div className="m-4 flex flex-col gap-2">
 
-                <div className={`relative bg-green-300 rounded-full`}>
 
-                    <input className="bg-transparent hover:cursor-pointer m-0  z-[2] absolute w-full h-full opacity-0" type="radio" />
-                    <p className="px-3 text-sm">Gastos</p>
+
+                <div className="flex gap-2 ">
+                    <div className="relative  rounded-full " >
+                        <input name="TransactionsType" type="radio" value="expenses" className="peer bg-transparent hover:cursor-pointer m-0 z-[2] absolute w-full h-full opacity-0" />
+                        <p className=" text-lg text-gray-300 peer-checked:font-bold peer-checked:text-white ">Gastos</p>
+                    </div>
+                    <div className="relative  rounded-full " >
+                        <input name="TransactionsType" type="radio" value="income" className="peer bg-transparent hover:cursor-pointer m-0 z-[2] absolute w-full h-full opacity-0" />
+                        <p className=" text-lg text-gray-300 peer-checked:font-bold peer-checked:text-white" >Ganhos</p>
+                    </div>
+                    <div className="relative  rounded-full " >
+                        <input name="TransactionsType" type="radio" value="transfer" className="peer bg-transparent hover:cursor-pointer m-0 z-[2] absolute w-full h-full opacity-0" />
+                        <p className="text-lg text-gray-300 peer-checked:font-bold peer-checked:text-white ">Transferência</p>
+                    </div>
+
                 </div>
-                <div className={`relative bg-green-300 rounded-full`}>
+                <div>
+                    <div className="flex flex-col">
+                        <p>Valor do ganho</p>
 
-                    <input className="bg-transparent hover:cursor-pointer m-0  z-[2] absolute w-full h-full opacity-0" type="radio" />
-                    <p className="px-3 text-sm">Gastos</p>
+                    </div>
+
                 </div>
-                <div className={`relative bg-green-300 rounded-full`}>
-
-                    <input className="bg-transparent hover:cursor-pointer m-0  z-[2] absolute w-full h-full opacity-0" type="radio" />
-                    <p className="px-3 text-sm">Gastos</p>
-                </div>
-
 
             </div>
         </div>
